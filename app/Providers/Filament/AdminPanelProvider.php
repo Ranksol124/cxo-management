201a8ba46beb5d640d8e82a7855a3ad7d41d0641
ranksol124 <ranksoltoolsdrive@gmail.com>
2025-10-31 16:59:38 +0500

    job post active notifications

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -51,7 +51,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->sidebarCollapsibleOnDesktop()
             ->databaseNotifications()
-            ->databaseNotificationsPolling('5s') // optional: auto-refresh every 5 seconds
+            ->databaseNotificationsPolling('5s') 
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             // ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
